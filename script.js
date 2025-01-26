6a771a3ca98b2a89b23448818f819e3a8401c5df
Lucas Ferreira <135566999+Devnomam-Inc@users.noreply.github.com>
2025-01-26 00:53:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,27 @@ document.getElementById("form-mudanca").addEventListener("submit", function (eve
       }
     );
 });
+
+                    self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('formulario-cache').then(function (cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './styles.css',
+        './script.js',
+        './manifest.json',
+        './icon-192x192.png',
+        './icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
