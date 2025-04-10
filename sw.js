ab400cc86f8a7589e07433de3ffb070c683fc4cf
Lucas Ferreira <135566999+Devnomam-Inc@users.noreply.github.com>
2025-04-10 02:02:57 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,34 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'app-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('app-cache').then(function(cache) {
-      return cache.addAll([
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/manifest.json',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
